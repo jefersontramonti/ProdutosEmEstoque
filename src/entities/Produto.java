20d6e89ed6493a04f2ec8a04b9a28641a64832fb
Jeferson Tramonti <jefersontramonti@outlook.com>
2020-11-30 21:34:09 -0300

    Uso de classes no projeto classe Produto que contem os atributos nome preço e quantidade, funções: valorTotalEmEstoque retorna o valor total do estoque. adccProduto onde adiciona + 1 no produto removeProduto retira -1 do estoque

--- src/entities/Produto.java
+++ src/entities/Produto.java
@@ -1,25 +1,25 @@
 package entities;
 
-public class Produto {
+public class Produto { //classe produto
 
     public String nome;
     public double preco;
     public int quantidade;
 
-    public double valorTotalEmEstoque() {
+    public double valorTotalEmEstoque() { //função que retorna o total no estoque
         return preco * quantidade;
     }
 
-    public void adccProdutos(int quantidade) {
+    public void adccProdutos(int quantidade) { // função que acrescenta ao estoque
         this.quantidade += quantidade;
 
     }
 
-    public void removeProduto(int quantidade) {
+    public void removeProduto(int quantidade) {//  função que retira do estoque
         this.quantidade -= quantidade;
     }
 
-    public String toString() {
+    public String toString() { //formatação do texto
         return nome
                 + ", R$ "
                 + String.format("%.2f", preco)
